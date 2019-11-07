4b682697be1eebd33d4200ecf26c8b436376372e
Synxsaints <Synxsaints@LAPTOP-EAMMNJ87>
2019-11-07 21:18:32 +0700

    First Commit

---
+++ src/pbo2/pkg10118060/latihan/ke/pkg57/vehicle/Skateboard.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118060.latihan.ke.pkg57.vehicle;
+
+/**
+ *
+ * @author Synxsaints
+ */
+public class Skateboard extends Vehicle {
+    private double myBoardLength;
+    
+    public Skateboard() {
+        System.out.println("Skateboard");
+    }
+
+    public double getMyBoardLength() {
+        return myBoardLength;
+    }
+
+    public void setMyBoardLength(double myBoardLength) {
+        this.myBoardLength = myBoardLength;
+    }
+    
+}
